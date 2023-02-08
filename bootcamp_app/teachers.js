db12d5e3680c6d79b58c0ab8a848e3f74171bbaa
Viet Tran <viettran101294@gmail.com>
2023-02-08 15:29:53 -0500

    Node Postgres completed

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,36 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const userInput = process.argv.slice(2);
+const queryTemplate = `
+SELECT
+  teachers.name as teacher,
+  cohorts.name as cohort,
+  COUNT(assistance_requests.*)
+FROM
+  assistance_requests
+  JOIN teachers ON teachers.id = assistance_requests.teacher_id
+  JOIN students ON students.id = assistance_requests.student_id
+  JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE
+  cohorts.name = $1
+GROUP BY
+  teachers.name,
+  cohorts.name
+ORDER BY
+  teachers.name;
+`;
+
+pool.query(queryTemplate, userInput)
+  .then(res => {
+    res.rows.forEach(teacher => {
+      console.log(`${teacher.cohort}: ${teacher.teacher}`);
+    });
+  })
+  .catch(err => console.error('query error', err.stack));
